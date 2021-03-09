117bc696aa0b7e7beb06d247bb2d981f6b0b73f3
Crystal Brown <crystalkeithbrown@gmail.com>
2021-03-09 11:58:24 -0500

    taskManager

--- task.js
+++ task.js
@@ -10,13 +10,36 @@ What would you like to do (Please enter one of the options below):
 "CLOSE" - Close The Task Manager
 `;
 
+// Array for storing tasks
+const tasks = [
+  `Charge MacBook`, 
+  `Master JavaScript`
+];
+
+// For displaying tasks for the user
+let showTasks = "";
+
 // Displays the menu for the user to select an option. Also, sets the user's response to the userInput variable
 let userInput = prompt(menu);
 
 // Loops and continues to display the menu until the user ends the program (AKA Until the user enters "CLOSE")
 while (userInput !== "CLOSE"){
+  // Checks if user entered "TASKS"
+  if (userInput === "TASKS"){
+    // Concatenates each task (AKA item) in the tasks array to the showTasks string variable
+    for (task of tasks){
+      // Using \n to create a new line (AKA Line Break) after each task
+      showTasks += `${task}\n`;
+    }
+    // Displays the current tasks to the user
+    alert(showTasks);
+    // Sets the value of the showTasks string variable back to an empty string
+    // showTasks = "";
+  }
+
   // Displays the menu again
   userInput = prompt(menu);
 }
 // Alerts the user that they have closed the program
 alert(`Thank you for using Task Manager!`);
+
